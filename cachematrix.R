7ae345123e89ab4372f62f1ec38f0156b6f8b47f
Seyi Adebayo <seyiaadebayo@gmail.com>
2017-02-13 00:32:37 -0500

    added code for existing functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 'makeCacheMatrix' is a fxn that takes a matrix as an argument
+## and caches its attributes in memory
+## 'cacheSolve' is a fxn that attempts to check for an object
+## in memory and if it exists, returns the inverse of it
 
-## Write a short comment describing this function
+## a function that persists the contents and attributes of
+## a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list( set = set, get = get, 
+          setinverse = setinverse, 
+          getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## a function that retrieves an object from memory and, if it exists,
+## gains access to its attributes 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message('getting cached data')
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
