0188f92b678710f7e8b156cc7f35cad013c67c97
Seyi Adebayo <seyiaadebayo@gmail.com>
2017-02-13 00:40:20 -0500

    provided additional comments in fxn bodies.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,7 @@
 ## in memory and if it exists, returns the inverse of it
 
 ## a function that persists the contents and attributes of
-## a matrix
+## a matrix while also producing its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
@@ -23,17 +23,21 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## a function that retrieves an object from memory and, if it exists,
-## gains access to its attributes 
+## gains access to its attributes while also reserving the capability
+## to populate a new matrix object with its own attributes
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
     m <- x$getinverse()
     if(!is.null(m)) {
         message('getting cached data')
         return(m)
     }
+    ## get new matrix
     data <- x$get()
+    ## take the inverse of the matrix
     m <- solve(data, ...)
+    ## set the new inverse
     x$setinverse(m)
     m
 }
